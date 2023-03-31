ac5063117055885f05c03b20997c3e21f1d12be2
Eduard Frank <eddy@scanbot.io>
2023-03-31 15:16:41 +0200

    add cordova-plugin-scanbot-barcode-scanner. basic example of the barcode scanner UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import ScanbotBarcodeSdk, { ScanbotBarcodeSDKConfiguration } from 'cordova-plugin-scanbot-barcode-scanner';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,24 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class AppComponent {
-  constructor() {}
+
+  // @ts-ignore
+  private SDK = ScanbotBarcodeSdk.promisify();
+
+  constructor() {
+    this.initScanbotSdk();
+  }
+
+  async initScanbotSdk() {
+    const options: ScanbotBarcodeSDKConfiguration = {
+      licenseKey: '',
+      loggingEnabled: true
+    };
+    try {
+      const result = await this.SDK.initializeSdk(options);
+      console.log(JSON.stringify(result));
+    } catch (err) {
+      alert(JSON.stringify(err));
+    }
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import ScanbotBarcodeSdk, { BarcodeScannerConfiguration } from 'cordova-plugin-scanbot-barcode-scanner';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,28 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class HomePage {
+  // @ts-ignore
+  private SDK = ScanbotBarcodeSdk.promisify();
+
   constructor() {}
+
+  async startBarcodeScanner() {
+    const licenseInfo = await this.SDK.getLicenseInfo();
+    if (!licenseInfo.isLicenseValid) {
+      // see licenseInfo.licenseStatus for more details
+      alert('Invalid or expired (trial) license!\n\n' + JSON.stringify(licenseInfo));
+      return;
+    }
+
+    const configs: BarcodeScannerConfiguration = {
+      topBarBackgroundColor: '#c8193c',
+      cancelButtonTitle: 'Abort',
+      //barcodeFormats: ['QR_CODE', 'EAN_13', 'EAN_8'],
+      // see further configs...
+    };
+    const result = await this.SDK.startBarcodeScanner(configs);
+    if (result.status === 'OK') {
+      alert(`${result.barcodes![0].type}: ${result.barcodes![0].text}`);
+    }
+  }
 }
