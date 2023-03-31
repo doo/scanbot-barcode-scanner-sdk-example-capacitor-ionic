0ce5d05445638830069bc66ba83dc5e52856fe75
Eduard Frank <eddy@scanbot.io>
2023-03-31 14:39:19 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.scanbot.example.sdk.barcode.capacitor',
+  appName: 'ScanbotBarcodeScannerExampleCapacitor',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
