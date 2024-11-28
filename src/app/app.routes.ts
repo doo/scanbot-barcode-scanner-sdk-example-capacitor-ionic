d57bd35a5d488c3bd1a1003f7682a965d659e369
StefanKScanbot <stefankrzhovski@scanbot.io>
2024-11-28 10:10:58 +0100

    Reverted legacy-barcode-results.page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,13 @@ export const routes: Routes = [
       ),
   },
   {
+    path: 'legacy-barcode-results/:results',
+    loadComponent: () =>
+      import(
+        './results/legacy-barcode-results/legacy-barcode-results.page'
+      ).then((m) => m.LegacyBarcodeResultsPage),
+  },
+  {
     path: 'barcode-results/:results',
     loadComponent: () =>
       import('./results/barcode-results/barcode-results.page').then(
