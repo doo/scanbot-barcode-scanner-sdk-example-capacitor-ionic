e527ab1c88e3869c53e193971c4ac82180f153c5
Marko <marko@scanbot.io>
2025-12-18 11:54:57 +0100

    Remove unused snippets

--- src/codeSnippets/init/quick-start.ts
+++
@@ -1,5 +0,0 @@
-import { ScanbotBarcodeSDK, SdkConfiguration } from 'capacitor-plugin-scanbot-barcode-scanner-sdk';
-
-ScanbotBarcodeSDK.initialize(new SdkConfiguration({ licenseKey: '' }))
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
