2a3f133dc8847c855cbbcae0a85ef16b8d9aaec7
StefanKScanbot <stefankrzhovski@scanbot.io>
2026-08-07 14:16:30 +0200

    Added code snippets for ar overlay in custom ui

---
+++ src/codeSnippets/custom-ui/ar-overlay-multi-scan.ts
@@ -0,0 +1,45 @@
+import {
+  BarcodeCustomUIComponent,
+  BarcodeCustomUIConfiguration,
+  BarcodeCustomUIResultHandlers,
+  BarcodeScannerConfiguration,
+} from 'capacitor-plugin-scanbot-barcode-scanner-sdk';
+
+
+function createAndAttachBarcodeScanner(element: HTMLElement) {
+  const barcodeCustomUIComponent = new BarcodeCustomUIComponent();
+
+  const resultCallbacks: BarcodeCustomUIResultHandlers = {
+    onBarcodeScannerResult: (result) => {
+      //Handle scanned barcodes
+    },
+    onError: (error) => {
+      //Handle errors during scanning
+    },
+  };
+
+  const initialConfiguration: BarcodeCustomUIConfiguration = {
+    // AR Overlay related configurations
+    overlayConfiguration: {
+      overlayEnabled: true,
+      textFormat: 'CODE_AND_TYPE',
+      polygonColor: '#00CFA633',
+      strokeColor: '#00CFA6CC',
+      textColor: '#ffffff',
+      textContainerColor: '#00CFA6CC',
+    },
+    // Barcode Scanner configurations
+    scannerConfiguration: new BarcodeScannerConfiguration({
+      optimizedForOverlays: true,
+    }),
+  };
+
+  barcodeCustomUIComponent.attachScannerOnElement(
+    element,
+    false,
+    resultCallbacks,
+    initialConfiguration,
+  );
+
+  return barcodeCustomUIComponent;
+}
